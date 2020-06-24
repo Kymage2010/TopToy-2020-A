ea01baaa384c333ed63c7ce1fb2a8cb00d189fce
Kymage2010 <maxim07@inbox.ru>
2020-06-24 18:06:47 +0300

    small tuning

--- js/es.js
+++ js/es.js
@@ -63,7 +63,7 @@ $(document).ready(function () {
     }
     /*header stick*/
     function Header() {
-        0 < $(window).scrollTop() ? ($("header").css("position", "sticky").css("box-shadow", "0 -3px 6px 4px var(--icon)").on("transitionend webkitTransitionEnd oTransitionEnd otransitionend")) : ($("header").css("position", "inherit").css("box-shadow", "none").on("transitionend webkitTransitionEnd oTransitionEnd otransitionend"))
+        0 < $(window).scrollTop() ? ($("header").css("position", "sticky").css("box-shadow", "0 -3px 6px 4px var(--icon)").css("padding", "0").on("transitionend webkitTransitionEnd oTransitionEnd otransitionend")) : ($("header").css("position", "inherit").css("box-shadow", "none").css("padding", "0.75rem 0").on("transitionend webkitTransitionEnd oTransitionEnd otransitionend"))
     }
     Header(), $(window).scroll(function () {
         Header()
